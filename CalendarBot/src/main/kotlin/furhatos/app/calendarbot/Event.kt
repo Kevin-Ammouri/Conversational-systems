812864344da60416683f8528269def276bc8ef2d
Kevin Ammouri <kammouri@kth.se>
2020-12-02 08:35:21 +0100

    updated readme

---
+++ CalendarBot/src/main/kotlin/furhatos/app/calendarbot/Event.kt
@@ -0,0 +1,12 @@
+package furhatos.app.calendarbot
+
+class Event {
+    var day: String? = null
+    var date: String? = null
+    var dayContext: String? = null
+    var startTime: String? = null
+    var endTime: String? = null
+    var timeOfTheDay: String? = null
+    var name: String? = null
+
+}
